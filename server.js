661bcd2f4f45aa34967d8b826c0ea83723baf016
Aaron Colbert <aaron-colbert@hotmail.com>
2018-08-25 15:35:54 -0400

    Models added, Routes, Scraper completed

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+var PORT = 3000;
+
+// Initialize Express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
+app.use(express.static("public"));
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/articlescraperdb");
+
+//Routes
+require("./routes/api-routes.js")(app);
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
